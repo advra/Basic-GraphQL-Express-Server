aa77d69595bf839ada62cfb570c4ad69c4798086
advra <enthusiasticnftartist@gmail.com>
2023-01-23 00:54:36 -0800

    Updated readmes

--- client/src/components/AddCollectionModal.jsx
+++ client/src/components/AddCollectionModal.jsx
@@ -50,7 +50,7 @@ function AddCollectionModal() {
         className=" text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" 
         type="button"
         >
-        Toggle modal
+        Add New Collection
       </button>
 
       {/* <!-- Main modal --> */}
